004d7fd452d4cc94dba265c83ad627df1ff8363b
Mazhar <nsrmazhar@gmail.com>
2023-01-07 12:39:15 +0530

    js added

---
+++ public/index.js
@@ -0,0 +1,6 @@
+function helloUser(){
+    // alert("in helloUser...");
+    let element = document.getElementById("hello-box")
+    element.innerHTML = "Hello Mazhar"
+    element.style.color = "green"
+}
